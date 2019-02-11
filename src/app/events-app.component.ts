945265511d784a5c1e12a4e4c45c602a26de6879
Colburn Sanders <colburnsanders@gmail.com>
2019-02-11 13:02:37 -0600

    updating static  files

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'events-app',
-  template: '<h2>Hello World</h2>',
+  template: `<h2>Hello World</h2>
+  <img src="/assets/images/basic-shield.png"/>
+  `,
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
