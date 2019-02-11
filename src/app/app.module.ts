d0330da0b3e7154beeb16391e60c950546724bff
Colburn Sanders <colburnsanders@gmail.com>
2019-02-11 15:09:19 -0600

    updating components and external templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventListComponent } from './events/events-list.component';
+import { EventThumbNailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventListComponent,
+    EventThumbNailComponent
   ],
   bootstrap: [EventsAppComponent]
 })
